f4bcdc56a88f8f956622f3d83b29733ee8f6d9e7
Saceanu Ionut <saceanu.ionut@icloud.com>
2023-12-26 16:09:43 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import { Sidebar } from "./components/sidebar/Sidebar";
 import { Topbar } from "./components/topbar/Topbar";
-import "./app.css";
+import "./App.css";
 import { Home } from "./pages/home/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from "react";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/users",
+      element: <></>,
+    },
+  ]);
+
   return (
     <div>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <RouterProvider router={router} />
       </div>
     </div>
   );
